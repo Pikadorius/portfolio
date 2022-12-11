65ec6c9d25b3c6a000e11bacff85338551407f0b
Pikadorius <karpukea@yandex.by>
2022-12-11 21:54:51 +0300

    new changes

--- src/components/Main/block3.projects/Projects.tsx
+++ src/components/Main/block3.projects/Projects.tsx
@@ -3,19 +3,31 @@ import s from './Projects.module.css'
 import Project from './Project/Project';
 import pr1 from '../../../assets/pics/1.jpg'
 import pr2 from '../../../assets/pics/2.jpg'
+import pr3 from '../../../assets/pics/3.jpg'
+
 
 type ProjectsType = {
     id: string
 }
 
-const Projects = (props:ProjectsType) => {
+const Projects = (props: ProjectsType) => {
     return (
         <div className={s.wrapper}>
             <div className={s.projectsCard}>
                 <div className={s.projectsCardName} id={props.id}><span>My projects</span></div>
                 <div className={s.projectsField}>
-                    <Project pic={pr1} name={'Counter'} description={'Counter with settings.'} link={'1'}/>
-                    <Project pic={pr2} name={'Todolist'} description={'Todolist on Redux.'} link={'2'}/>
+                    <Project pic={pr1}
+                             name={'Social Network'}
+                             description={'Social network without graphic design. Used class and functional components, Redux'}
+                             link={'https://pikadorius.github.io/Samurai-way-SN/'}/>
+                    <Project pic={pr2}
+                             name={'Todolist'}
+                             description={'Todolist on Redux. Used universal components and Material UI.'}
+                             link={'https://pikadorius.github.io/todolist4/'}/>
+                    <Project pic={pr3}
+                             name={'Counter'}
+                             description={'Counter or Redux with Dan Abramov method \n to save and load loaclStorage data.'}
+                             link={'https://pikadorius.github.io/counter3/'}/>
                 </div>
             </div>
         </div>
