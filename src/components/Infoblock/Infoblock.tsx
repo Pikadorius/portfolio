a1940e8c5eb3970185cc7891be0a9b5d55ae492c
Pikadorius <karpukea@yandex.by>
2023-01-27 21:17:10 +0300

    added some animations

--- src/components/Infoblock/Infoblock.tsx
+++ src/components/Infoblock/Infoblock.tsx
@@ -6,7 +6,7 @@ import AnimatedText from 'react-animated-text-content';
 import Fade from '@mui/material/Fade';
 
 const Infoblock = () => {
-    const init = ['Front-End Developer', 'Egor Karpuk', 'Strong specialist' , 'Ready to work']
+    const init = ['Front-End Developer', 'Egor Karpuk', 'Experienced specialist', 'Ready to work']
     const [text, setText] = useState(init[0])
 
     useEffect(() => {
@@ -24,33 +24,35 @@ const Infoblock = () => {
         <aside className={s.wrapper}>
 
             <Fade appear={true} in={true} timeout={6000}>
-            <div className={s.infoBlock}>
-                <div className={s.border1}></div>
-                <div className={s.border2}></div>
-                <img className={s.avatar} src={myself} alt="me"/>
-                <h3>Hi there!</h3>
-                <h3>I am</h3>
-                <AnimatedText
-                    type="words" // animate words or chars
-                    animation={{
-                        x: '200px',
-                        y: '-20px',
-                        scale: 1.1,
-                        ease: 'ease-in-out',
-                    }}
-                    animationType='lights'
-                    interval={0.06}
-                    duration={0.8}
-                    tag="h2"
-                    className={s.shining}
-                    includeWhiteSpaces
-                    threshold={0.1}
-                    rootMargin="20%"
-                >
-                    {text}
-                </AnimatedText>
-                <h3 style={{zIndex:'1'}}><a className={common.superLink} href={'#contacts'}>Hire me</a></h3>
-            </div>
+                <div className={s.infoBlock}>
+                    <div className={s.border1}></div>
+                    <div className={s.border2}></div>
+                    <img className={s.avatar} src={myself} alt="me"/>
+                    <h3>Hi there!</h3>
+                    <h3>I am</h3>
+                    <AnimatedText
+                        type="words" // animate words or chars
+                        animation={{
+                            x: '200px',
+                            y: '-20px',
+                            scale: 1.1,
+                            ease: 'ease-in-out',
+                        }}
+                        animationType='lights'
+                        interval={0.06}
+                        duration={0.8}
+                        tag="h2"
+                        className={s.shining}
+                        includeWhiteSpaces
+                        threshold={0.1}
+                        rootMargin="20%"
+                    >
+                        {text}
+                    </AnimatedText>
+                    <h1 className={s.hireLink} style={{zIndex: '1'}}><a className={`${common.superLink}`} href={'#contacts'}>
+                        HIRE ME
+                    </a></h1>
+                </div>
             </Fade>
         </aside>
     )
