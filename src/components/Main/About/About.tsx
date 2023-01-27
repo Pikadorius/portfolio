2a77ee185c054ffcc52f149a168ec067f64e37cc
Pikadorius <karpukea@yandex.by>
2023-01-27 21:27:15 +0300

    change about me block

--- src/components/Main/About/About.tsx
+++ src/components/Main/About/About.tsx
@@ -15,18 +15,17 @@ const About = (props: AboutType) => {
                 <div className={s.infoText}>
                     <h3 className={s.subtitle}>About me</h3>
                     <h2 className={s.title}>Front-end Developer</h2>
-                    <p className={s.desc}>I'm frontend developer with
-                        experience in creating SPA using
-                        React, Redux, TypeScript.
-                        Knowledge in user interface,
-                        testing, and debugging processes. I
-                        improving my skills in this direction
-                        and plan to expand them with new
-                        technologies, such as Angular.
-                        I usually spend my free time
-                        reading IT-literature (M.Fauler),
-                        Codewars and improving my
-                        English. Open to your suggestions</p>
+                    <p className={s.desc}>I'm Front-End developer with experience in creating SPA using React,
+                        Redux/Redux Toolkit, TypeScript.
+                        I am improving my skills in this direction and plan to expand them with new technologies, such
+                        as Angular or React Native.
+                        In the future I plan to become a full-stack developer, because I love learning new things and
+                        exercising my brain :)
+                        In my free time I like to read, solve some katas on Codewars, ride a bicycle and spent time with
+                        my family.
+                        Now I am also improving my English (my current level - pre-intermediate).
+
+                        Open to your suggestions</p>
                 </div>
             </div>
         </section>
