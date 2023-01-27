6323722b7acc3947be6f8f0ec1f0b8e5421dc033
Pikadorius <karpukea@yandex.by>
2023-01-27 21:35:08 +0300

    change about me block css

--- src/components/Main/About/About.tsx
+++ src/components/Main/About/About.tsx
@@ -16,16 +16,18 @@ const About = (props: AboutType) => {
                     <h3 className={s.subtitle}>About me</h3>
                     <h2 className={s.title}>Front-end Developer</h2>
                     <p className={s.desc}>I'm Front-End developer with experience in creating SPA using React,
-                        Redux/Redux Toolkit, TypeScript.
-                        I am improving my skills in this direction and plan to expand them with new technologies, such
-                        as Angular or React Native.
-                        In the future I plan to become a full-stack developer, because I love learning new things and
-                        exercising my brain :)
-                        In my free time I like to read, solve some katas on Codewars, ride a bicycle and spent time with
-                        my family.
-                        Now I am also improving my English (my current level - pre-intermediate).
+                        Redux/Redux Toolkit, TypeScript.I am improving my skills in this direction and plan to expand them with new
+                        technologies, such
+                        as Angular or React Native.</p>
+                    <p className={s.desc}>In the future I plan to become a full-stack developer, because I love learning
+                        new things and
+                        exercising my brain :)</p>
+                    <p className={s.desc}>In my free time I like to read, solve some katas on Codewars, ride a bicycle
+                        and spent time with
+                        my family.</p>
+                    <p className={s.desc}>Now I am also improving my English (my current level - pre-intermediate).</p>
 
-                        Open to your suggestions</p>
+                    <h4>Open to your suggestions</h4>
                 </div>
             </div>
         </section>
