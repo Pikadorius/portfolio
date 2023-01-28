9319b5c9e644a7a4a486fced98d2799ec821cca1
Pikadorius <karpukea@yandex.by>
2023-01-28 10:12:08 +0300

    deleted unused

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,8 @@ import Infoblock from './components/Infoblock/Infoblock';
 import HideAppBar from './common/AppBar/AppBar';
 import HeroBanner from './test/test';
 
-/*
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import { styled } from '@mui/material/styles';
-import Fab from '@mui/material/Fab';
-import Box from '@mui/material/Box';
-*/
 
 function App() {
-
-    /*const StyledFab = styled(Fab)({
-        position: 'absolute',
-        zIndex: 1,
-        top: -30,
-        left: 0,
-        right: 0,
-        margin: '0 auto',
-    });*/
 
     return (
         <div className="App" id={'root'}>
@@ -41,23 +20,6 @@ function App() {
                 <Main/>
             </div>
 
-            {/*<AppBar>
-                <Toolbar>
-                    <IconButton color="inherit" aria-label="open drawer">
-                        <MenuIcon/>
-                    </IconButton>
-                    <StyledFab color="secondary" aria-label="add">
-                        <AddIcon/>
-                    </StyledFab>
-                    <Box sx={{flexGrow: 1}}/>
-                    <IconButton color="inherit">
-                        <SearchIcon/>
-                    </IconButton>
-                    <IconButton color="inherit">
-                        <MoreIcon/>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>*/}
         </div>
     );
 }
